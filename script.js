ff8cce07820450dbfd1d6ed60ea4e34da0e24abd
SkyLinux <85138227+SkyServer21@users.noreply.github.com>
2021-07-24 04:26:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,14 @@ const navSlide = () => {
 	}
 
 window.onload = () => navSlide();
+
+
+<script>
+
+var api= "dc0e30b1d3ae9e7f002522b3d41837a6dcd0cf82";
+
+var ct= "1";
+
+</script>
+
+<script src="//aylink.co/t89s3.js"></script>
