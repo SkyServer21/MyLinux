38254c81621da2e9349009a94f364b68719cb122
SkyLinux <85138227+SkyServer21@users.noreply.github.com>
2021-08-11 20:30:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ $(window).on('scroll', function(){
 	if($(window).scrollTop()){
       $('header').addClass('nav-show');
 		  
-	} 
+	} <a target="_blank"href="https://faucetcrypto.com/ref/228864"><imgsrc="https://faucetcrypto.com/banners/300x250.gif"width="300" height="250"></a>
 	else{
 		$('header').removeClass('nav-show');
 	}
 	   
 }
-
+<a target="_blank"href="https://faucetcrypto.com/ref/228864"><imgsrc="https://faucetcrypto.com/banners/300x250.gif"width="300" height="250"></a>
 //hamburger
 const navSlide = () => {
 	 const hamburger = document.querySelector(".hamburger");
@@ -19,7 +19,7 @@ const navSlide = () => {
      hamburger.onclick = () => {
 		
 	 navbar.classList.toggle("nav-active");
-		 
+<a target="_blank"href="https://faucetcrypto.com/ref/228864"><imgsrc="https://faucetcrypto.com/banners/300x250.gif"width="300" height="250"></a>		 
       //Animation links
 	 navLinks.forEach((link, index) => {
 		if (link.style.animation) {
@@ -29,12 +29,9 @@ const navSlide = () => {
 		   }
 		});
 	  //hamburger animation
-	     <a target="_blank"
-href="https://faucetcrypto.com/ref/228864"><img
-src="https://faucetcrypto.com/banners/300x250.gif"
-width="300" height="250"></a>
 
-	     
+
+	<a target="_blank"href="https://faucetcrypto.com/ref/228864"><imgsrc="https://faucetcrypto.com/banners/300x250.gif"width="300" height="250"></a>     
 	     
 	 hamburger.classList.toggle("toggle");             
 	 
